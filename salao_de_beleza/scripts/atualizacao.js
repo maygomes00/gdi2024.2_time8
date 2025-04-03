cd636e319f4a105b44f9152471dec08834e07653
Cafeko <caiofgs2329@gmail.com>
2025-04-03 12:21:48 -0300

    Atualização: insertOne serviço e profissional

--- salao_de_beleza/scripts/atualizacao.js
+++ salao_de_beleza/scripts/atualizacao.js
@@ -12,15 +12,27 @@ db.clientes.updateOne(
     { $set: { telefone: "555-3333" } }
   );
 
-  // Adicionar um serviço à lista de um profissional (ADDTOSET)
+// Adicionar um serviço à lista de um profissional (ADDTOSET)
 db.profissionais.updateOne(
     { nome: "Joana" },
     { $addToSet: { servicos: "Hidratação Profunda" } }
   );
   
-  // Atualizar múltiplos profissionais para incluir "Sobrancelhas" como serviço (UPDATEMANY)
+// Atualizar múltiplos profissionais para incluir "Sobrancelhas" como serviço (UPDATEMANY)
 db.profissionais.updateMany(
     { disponibilidade: { $all: ["Segunda", "Terça"] } },
     { $addToSet: { servicos: "Sobrancelha Design + Henna" } }
   );
-  
+
+// Adiciona corte de cabela masculino como novo serviço (INSERTONE)
+db.servicos.insertOne({
+  "nome": "Corte de Cabelo Masculino",
+  "preco": 40.00
+})
+
+// Adiciona um novo profissional que realiza os cortes de cabelo masculino (INSERTONE)
+db.profissionais.insertOne({
+  "nome": "João",
+  "disponibilidade": ["Sabado", "Domingo"],
+  "servicos": ["Corte de Cabelo Masculino"]
+})
